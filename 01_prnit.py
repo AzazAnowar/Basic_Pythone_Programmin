1c205053671e4ab1b26eb98ba19796308584287d
AzazAnowar <azazanowar@gmail.com>
2024-11-29 21:48:28 +0600

    first commit

---
+++ 01_prnit.py
@@ -0,0 +1,8 @@
+print("hi, how are you ")
+print("I think this is your first Python code ::) ")
+print("     /|")
+print("    / |")
+print("   /  |")
+print("  /   |")
+print(" /    |")
+print("/_____|")
