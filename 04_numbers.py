bc382b97b696813efd12e03f54410d3e8492dee0
AzazAnowar <azazanowar@gmail.com>
2024-12-02 01:46:02 +0600

    input

--- 04_numbers.py
+++ 04_numbers.py
@@ -1,3 +1,5 @@
+from math import *
+
 print(4 + 5)
 print(4 - 5)
 print(4 * 5)
@@ -9,3 +11,12 @@ print(20 % 2)
 my_num = 5
 print(str(my_num) + " is my favorite number")
 #print(my_num + " is my favorite number")
+
+my_num2 = -20
+print(abs(my_num2))
+print(pow(2,3))
+print(max(10,20,30,40,50))
+print(min(10,20,30,40,50))
+print(round(3.7))
+print(sqrt(36))
+print(int(sqrt(36)))
---
+++ 05_get_Input_From_User.py
@@ -0,0 +1,5 @@
+name = input("Enter your Name: ")
+age = input("Enter your Age: ")
+paisa = input("Enter your Paisa: ")
+
+print("You are "+name+ ". You are "+age+" years old and you have "+paisa+" BDT")
