301e136a1204c987f272912e3d646b9e56d4cd5d
AzazAnowar <azazanowar@gmail.com>
2024-12-02 23:57:54 +0600

    list

---
+++ 07_lists.py
@@ -0,0 +1,8 @@
+friends = ["Abul","Babul","Chabul","Dabul"]
+#print(friends)
+#print(friends[1])
+#print(friends[-1])
+#print(friends[-2])
+print(friends[1:3])
+best_friend = friends[1:3]
+print(best_friend)
