9afb25e3d983e23f328a986c036cff068482758c
AzazAnowar <azazanowar@gmail.com>
2024-12-04 00:18:46 +0600

    revise

--- 03_strings.py
+++ 03_strings.py
@@ -1,4 +1,28 @@
 """
+"""
+
+str1 = "Python Programming"
+str2 = "azaz anowar real"
+
+# Type of str1 and str2
+print(str1," is of type ", type(str1))
+print(str2," is of type ", type(str2))
+
+#targeting an element by index number [index number stat from 0]
+print(str1[5])
+# -5 => meaning start from end. when - index start from -1
+print(str1[-5])
+
+#slice start index : end index 0-4th element
+print(str1[0:5])
+
+# find the index
+print(str2.find("w"))
+
+#if the element is not found then -1
+print(str2.find("c"))
+
+
 #print("kaindo na babu \n kaindo na")
 # \n for new line
 #print("kaindo na babu \"kaindo na\"")
@@ -12,12 +36,3 @@ print(gan.upper())
 print(len(gan))
 print(gan.index("Re"))
 print(gan.replace("Re","Pagla"))
-
-"""
-
-str1 = "Python Programming"
-str2 = "azaz anowar real"
-
-# Type of str1 and str2
-print(str1," is of type ", type(str1))
-print(str2," is of type ", type(str2))
--- 07_lists.py
+++ 07_lists.py
@@ -6,3 +6,16 @@ friends = ["Abul","Babul","Chabul","Dabul"]
 print(friends[1:3])
 best_friend = friends[1:3]
 print(best_friend)
+
+# List Data Types
+split1 = "This,is,Orange"
+l1 = split1.split(',')
+print(l1)
+print(type(split1))
+print(type(l1))
+
+list2 = ['BD','BDT','+880']
+list3 = ['US','USD','+1']
+
+list2 += list3
+print(list2)
