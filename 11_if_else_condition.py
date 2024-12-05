06f3d72b01ae0c77ea198570f11bfca110e74d17
AzazAnowar <azazanowar@gmail.com>
2024-12-05 23:10:04 +0600

    loops

--- 11_if_else_condition.py
+++ 11_if_else_condition.py
@@ -7,3 +7,24 @@ elif is_tall and is_male:
     print("You a tall and male")
 else:
     print("your are not a Male and Short")
+
+
+
+a = 43
+b = 111
+c = 737
+"""
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+"""
+
+if a > b and a > c:
+    print("a is the largest number of all.")
+elif b > c and b > c:
+    print("b is the largest number of all.")
+else:
+    print("c is the largest number of all.")
---
+++ 13_for_loop.py
@@ -0,0 +1,14 @@
+for x in range(0, 6):
+    print(x)
+
+
+for x in range(2, 6):
+    print(x)
+
+
+#loops in list
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
